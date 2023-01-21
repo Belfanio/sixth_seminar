c21eb7751e6bc4add64ca9d6025d847b0104cbf8
Ulitin Sergey <ulitin_sergey@mail.ru>
2023-01-21 13:46:06 +0300

    end work

---
+++ 1 задание/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write("Введите числа через пробел: ");
+string numbers = Console.ReadLine();
+if (numbers == "")
+{
+    Console.WriteLine("Ввели пустую строку");
+}
+else
+{
+    string[] num = numbers.Split(' ');
+    if (num.try)
+    int[] ints = Array.ConvertAll(num, s => int.Parse(s));
+    int positive = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (ints[i] > 0)
+        {
+            positive += 1;
+        }
+    }
+
+    if (positive > 0)
+    {
+        Console.WriteLine($"Вы ввели {positive} чисел больше ноля");
+    }
+    else
+    {
+        Console.WriteLine("Вы не ввели ни одного числа, больше ноля");
+    }
+}
---
+++ 2 задание/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+double GetNumber(string message)
+{
+    double result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+double b1 = GetNumber("Введите b1 - ");
+double k1 = GetNumber("Введите k1 - ");
+double b2 = GetNumber("Введите b2 - ");
+double k2 = GetNumber("Введите k2 - ");
+
+if ((b1 == b2) && (k1 == k2)) {Console.WriteLine("Прямые совпадают");}
+else if (k1 == k2) {Console.WriteLine("Прямые параллельны");}
+else
+{
+double x=(b2-b1)/(k1-k2);
+
+double y=(k1*(b2-b1))/(k1-k2)+b1;
+
+    Console.WriteLine($"Точка пересечения прямых находиться в координате [{x},{y}] ");
+}
